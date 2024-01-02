36f2f213f929d9ceb5e0466d98048e027132c890
MarianeBS <mariane.souza22@etec.sp.gov.br>
2024-01-02 11:20:29 -0300

    Utilizando condicional IF

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -3,24 +3,7 @@ namespace projeto.Models
     //É recomendável que o nome do arquivo obedeça ao nome da classe. 
     public class Pessoa
     {
-        //Definindo propriedades:
-        //Nunca abreviar nomes de propriedades (não recomendável).
-        public string Nome { get; set; }
-        public int Idade { get; set; }   
-
-        //Criando o método/ação Apresentar:
-        public void Apresentar()
-        {
-
-            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
-
-            //Para realizar quebra de linha para melhor organização do código:
-            //Console.WriteLine($"Olá, meu nome é" + 
-            //$"{Nome}, e tenho {Idade} anos");
-
-            //Para pular uma linha no texto:
-            //Console.WriteLine($"Olá, meu nome é {Nome}, \n e tenho {Idade} anos");
-        }
+        
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,76 +1,27 @@
 ﻿//Caminho para a classe; o namespace:
 using projeto.Models;
 
-string apresentacao = "Olá, seja bem-vindo!";
-int quantidade = 1;
-double altura = 1.80;
-decimal preco = 1.80M;
-bool condicao = true;
-DateTime dataAtual = DateTime.Now;
-//DateTime adicionando 5 dias:
-DateTime dataCincoDias = DateTime.Now.AddDays(5);
+int quantidadeEstoque = 10;
+int quantidadeCompra = 0;
+bool possivelVenda = quantidadeCompra > 0 && quantidadeEstoque >= quantidadeCompra;
 
-int a = 10;
-int b = 20;
-int c = a + b;
+Console.WriteLine($"Quantidade em estoque: {quantidadeEstoque}");    
+Console.WriteLine($"Quantidade na compra: {quantidadeCompra}");    
+Console.WriteLine($"É possível realizar a venda?: {possivelVenda}");    
 
-//As duas sintaxes abaixo corresponderão ao mesmo resultado:
-//c = c + 5;
-//c += 5;
+// = Operador de atribuição
+// == Comparação
 
-//Cast ou Casting
-//Convertendo uma String em Inteiro:
-//Convert - Conversão para inteiros, podendo conter o valor nulo (null) 
-int d = Convert.ToInt32("5");
+if (quantidadeCompra == 0)
+{
+    Console.WriteLine("Venda inválida");
+}
 
-int e = int.Parse("5");
-
-//Para converter inteiros em strings:
-//int inteiro = 5;
-//string a = inteiro.ToString();
-
-//Para converter strings em inteiros:
-//string h = "5";
-//int i = Convert.ToInt32(h);
-
-//Cast Simples - Conversão automática:
-//Um inteiro cabe em um double
-//int f = 5;
-//double g = f;
-
-
-Console.WriteLine(apresentacao);
-Console.WriteLine("Valor da variável quantidade: " + quantidade);
-Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
-Console.WriteLine("Valor da variável preço: " + preco);
-Console.WriteLine("Valor da variável condição: " + condicao);
-Console.WriteLine("Data atual: " + dataAtual);
-Console.WriteLine("Data atual com mais cinco dias: " + dataCincoDias);
-//Apresentando modelo com apenas a data, utilizando o DateTime:
-Console.WriteLine("Data atual com mais cinco dias s/ horário: " + dataCincoDias.ToString("dd/MM/yyyy"));
-//Apresentando modelo sem os milissegundos, utilizando o DateTime:
-Console.WriteLine("Data atual com mais cinco dias s/ milissegundos: " + dataCincoDias.ToString("dd/MM/yyyy HH:mm"));
-//Resultado da soma:
-Console.WriteLine("Resultado da soma: " + c);
-//Conversão de String para Int:
-Console.WriteLine("Número convertido: " + d);
-//Conversão de String para Int com Parse:
-Console.WriteLine("Número convertido: " + e);
-
-
-
-//Instância da classe Pessoa:
-//Pessoa = classe (como a planta de uma casa)
-//p = variável (endereço na memória)
-//new Pessoa() = (como a planta de uma casa já construída)
-Pessoa p = new Pessoa();
-
-//Atribuindo valores às variáveis:
-p.Nome = "Mariane Souza";
-p.Idade = 18;
-
-//Chamando o método Apresentar:
-//O ponto "." representa a continuidade de uma ação
-p.Apresentar();
-
-
+else if (possivelVenda)
+{
+    Console.WriteLine("Venda realizada com sucesso!");    
+}
+else
+{
+    Console.WriteLine("Quantidade insuficiente no estoque!");
+}
